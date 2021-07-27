5432398479e80f4ab5f49b7e2d4c888061a2e53a
JunmingJiang <840005617@qq.com>
2021-07-27 15:18:25 +0800

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='pmrvc',
+	version='0.0.1',
+	description='Python modules runtime version check',
+	author='zzztttkkk',
+	author_email='ztkisalreadytaken@gmail.com',
+	url='https://github.com/zzztttkkk/pmrvc',
+	packages=['pmrvc'],
+)
